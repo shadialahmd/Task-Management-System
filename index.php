f2ff4ea35feeae211345b914e14869edbaad81cd
shadialahmd <shadi.alahmd@gmail.com>
2021-11-02 11:27:41 +0400

    Update index.php

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2021 <a href="https://www.itsourcecode.com/">itsourcecode.com</a>.</strong>
+    <strong>Copyright &copy; 2021 <a href="#"></a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b><?php echo $_SESSION['system']['name'] ?></b>
